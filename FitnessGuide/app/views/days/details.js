e7fb8e8af4f1685956bd231012ff868d08b4eab8
emcako <emcako@abv.bg>
2016-02-20 21:56:57 +0200

    Pinch in and double tap added in details page for description!

--- FitnessGuide/app/views/days/details.js
+++ FitnessGuide/app/views/days/details.js
@@ -5,6 +5,23 @@ function onNavigatedTo(args){
 
 	var page = args.object;
 	page.bindingContext = page.navigationContext;
+
+	var label = page.getViewById("explanation");
+	label.fontSize = 14;
+
+	label.on('doubleTap', function (args) {
+
+		label.fontSize = 14;
+	});
+
+	label.on('pinch', function (args) {
+
+		if (label.fontSize <30 ) {
+			label.fontSize += args.scale;
+		}
+		
+	});
+
 }
 
 exports.onNavigatedTo = onNavigatedTo;
