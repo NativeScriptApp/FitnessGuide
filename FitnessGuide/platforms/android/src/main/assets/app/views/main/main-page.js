833d9cc1a7bee31c0f530c4a1f3e90986a76581b
emcako <emcako@abv.bg>
2016-02-20 17:37:20 +0200

    Notification step one!

--- FitnessGuide/platforms/android/src/main/assets/app/views/main/main-page.js
+++ FitnessGuide/platforms/android/src/main/assets/app/views/main/main-page.js
@@ -2,6 +2,11 @@
 //var actionBarModule = require("ui/action-bar");
 //var frameModule = require("ui/frame");
 //var pagesModule = require("ui/page");
+ var pushPlugin = require("nativescript-push-notifications");
+ var self = this;
+
+
+
 var view = require("ui/core/view");
 var pageNavigator = require("~/common/page-navigator");
 
@@ -15,8 +20,19 @@ var pageModules = (function() {
 		    segmentedBar = view.getViewById(page, "segments");
             segmentedBar.selectedIndex = 0;
             pageNavigator.attachNavigation(segmentedBar);
-		}
-	}
+
+
+           //notifications
+            pushPlugin.register({ senderID: 'yyiYli6L5Kpr4DxaNdzBxX8sw4PzS28PMIOuaywU' }, function (data){
+            	self.set("message", "" + JSON.stringify(data));
+            }, function() { });
+
+            pushPlugin.onMessageReceived(function callback(data) {	
+            	self.set("message", "" + JSON.stringify(data));
+            });
+
+        }
+    }
 
 	return pageModules;
 })();
