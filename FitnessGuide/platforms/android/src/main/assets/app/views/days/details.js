ab398d81c23abc54b65e8c16b9cf969ae10472d5
emcako <emcako@abv.bg>
2016-02-21 12:54:49 +0200

    Animation added for showing the details description!

--- FitnessGuide/platforms/android/src/main/assets/app/views/days/details.js
+++ FitnessGuide/platforms/android/src/main/assets/app/views/days/details.js
@@ -8,6 +8,22 @@ function onNavigatedTo(args){
 
 	var label = page.getViewById("explanation");
 	label.fontSize = 14;
+    label.animate({opacity:0,
+                   duration: 0});
+	label.animate({
+            translate: {
+                x: 0,
+                y: -40
+            },
+            opacity: 1,
+            duration: 2000,
+            translate: {
+                x: 0,
+                y: 40
+            },
+        });
+
+
 
 	label.on('doubleTap', function (args) {
 
