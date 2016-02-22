478cc2e07a43b04ffe484fc02de8109cf077abb1
zvet80 <zvet80@hotmail.com>
2016-02-22 18:20:30 +0200

    Added some food details

--- FitnessGuide/platforms/android/src/main/assets/app/views/food/foodDetails-view-model.js
+++ FitnessGuide/platforms/android/src/main/assets/app/views/food/foodDetails-view-model.js
@@ -6,6 +6,8 @@ class FoodDetailsViewModel extends Observable {
     super();
     this.title = food.firstFood;
     this.products = food.firstFood + ", "+ food.secondFood + ", "+ food.thirdFood;
+    this.content = "Proteins: " + food.proteins + " Carbs: "+food.carbohydrates +" Fats: "+ food.fats;
+    this.calories = "Calories: " + food.calories;
     this.explanation = food.explanation;
     this.picture = food.picture;
   }
