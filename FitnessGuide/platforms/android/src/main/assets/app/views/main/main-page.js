e4f12c801fe08c9a0dbc2d21e51d4618859f2122
emcako <emcako@abv.bg>
2016-02-23 04:26:30 +0200

    Notifications!

--- FitnessGuide/platforms/android/src/main/assets/app/views/main/main-page.js
+++ FitnessGuide/platforms/android/src/main/assets/app/views/main/main-page.js
@@ -1,12 +1,4 @@
-//var vmModule = require("./main-view-model");
-//var actionBarModule = require("ui/action-bar");
-//var frameModule = require("ui/frame");
-//var pagesModule = require("ui/page");
-// var pushPlugin = require("nativescript-push-notifications");
-// var self = this;
-
-
-
+//var backgroundTasks = require("background-tasks");
 var view = require("ui/core/view");
 var pageNavigator = require("~/common/page-navigator");
 
@@ -22,14 +14,51 @@ var pageModules = (function() {
             pageNavigator.attachNavigation(segmentedBar);
 
 
-           // //notifications
-           //  pushPlugin.register({ senderID: '3LbP5dTwALBb0xjlMlqXQ52VIUVHLZxNQGXfNvVS' }, function (data){
-           //  	self.set("message", "" + JSON.stringify(data));
-           //  }, function() { });
+       
 
-           //  pushPlugin.onMessageReceived(function callback(data) {	
-           //  	self.set("message", "" + JSON.stringify(data));
-           //  });
+        var app = require('application');
+        var packageName = app.android.context.getPackageName();
+        var launchIntent = app.android.context.getPackageManager().getLaunchIntentForPackage(packageName);
+        var className = launchIntent.getComponent().getClassName();
+        var tns = className + '.class';
+
+        var intent = new android.content.Intent(app.android.context, eval(tns));
+
+        // var pIntent = android.app.PendingIntent.getActivity(app.android.context, 0, intent, 0);
+        var pIntent;
+        var resImg = app.android.context.getResources().getIdentifier('icon' , 'drawable', app.android.context.getPackageName());
+
+        var mBuilder = new android.support.v4.app.NotificationCompat.Builder(app.android.context);
+        mBuilder.setSmallIcon(resImg);
+        mBuilder.setContentTitle('Fitness Guide');
+        mBuilder.setContentText('It is time to change your program !');
+        mBuilder.addAction(resImg, 'Do it !', pIntent);
+
+        var mNotificationId = 001;
+        var sytemservice = app.android.context.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+        
+        
+
+        sytemservice.notify(mNotificationId, mBuilder.build());
+        
+//         backgroundTasks.doInBackground(function() {
+//           timer.setInterval(function () {
+
+// //------------------------------------------------------------------------------
+
+//               //tuka e mqstoto kadeto trqbva da se buildva notifikaciqta s background task
+//               // prez opredeleno vreme , no ne namira modula background-task...
+            
+// //-------------------------------------------------------------------------------
+//             console.log("OK");   
+//             }, 5000);
+
+//            }).then(function(data){
+//               // data = result that you return from callback above
+//            }).catch(function(exception) {
+//              // exception = native exception or exception that you throw in callback
+//        });
+
 
         }
     }
